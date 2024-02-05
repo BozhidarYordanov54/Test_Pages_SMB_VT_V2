12b93e26dd4b96061d2f9dfb1ad5b564bb47f2f5
Bozhidar Yordanov <108609381+BozhidarYordanov54@users.noreply.github.com>
2024-02-06 01:58:20 +0200

    Implemented dark mode for the navbar and site body

--- js_design/darkmode.js
+++ js_design/darkmode.js
@@ -1,11 +1,34 @@
 var darkModeSwitch = document.getElementById("checkbox");
 
+// Check local storage for dark mode preference
+var savedDarkMode = localStorage.getItem('darkMode');
+
+// Set initial dark mode state based on the saved preference
+if (savedDarkMode === "true") {
+    darkModeSwitch.checked = true;
+    document.body.classList.add("darkmode");
+    document.querySelector('.nav-wrapper').classList.add("darkmode");
+    document.querySelector('.logo').src = "images/Logo-White.png";
+} else {
+    darkModeSwitch.checked = false;
+    document.body.classList.remove("darkmode");
+    document.querySelector('.nav-wrapper').classList.remove("darkmode");
+    document.querySelector('.logo').src = "images/Logo-Black.png";
+}
+
 darkModeSwitch.addEventListener("change", function() {
     if (darkModeSwitch.checked) 
     {
-        document.body.classList.add("dark-mode");
+        document.body.classList.add("darkmode");
+        document.querySelector('.nav-wrapper').classList.add("darkmode");
+        document.querySelector('.logo').src = "images/Logo-White.png";
     } else 
     {
-        document.body.classList.remove("dark-mode");
+        document.body.classList.remove("darkmode");
+        document.querySelector('.nav-wrapper').classList.remove("darkmode");
+        document.querySelector('.logo').src = "images/Logo-Black.png";
     }
+
+    // Save the dark mode state to local storage
+    localStorage.setItem('darkMode', darkModeSwitch.checked);
 });
