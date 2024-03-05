ed231d1e78b1b904009ad13b4bc1d309e1a486ef
Bozhidar Yordanov <108609381+BozhidarYordanov54@users.noreply.github.com>
2024-03-05 17:56:15 +0200

    rewritten the change password icon logic

--- js_design/changePasswordIcon.js
+++ js_design/changePasswordIcon.js
@@ -1,39 +1,47 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var showPasswords = document.querySelectorAll('.show-password');
-    var hidePasswords = document.querySelectorAll('.hide-password');
+    var showPasswordIcons = document.querySelectorAll('.fa-eye');
+    var hidePasswordIcons = document.querySelectorAll('.fa-eye-slash');
     var passwordInputs = document.querySelectorAll('input[type="password"]');
 
-    passwordInputs.forEach(function(input) {
+    passwordInputs.forEach(function(input, index) {
+        var showPasswordIcon = showPasswordIcons[index];
+        var hidePasswordIcon = hidePasswordIcons[index];
+
+        // Hide both icons initially
+        showPasswordIcon.style.fontSize = '0px';
+        hidePasswordIcon.style.fontSize = '0px';
+
         input.addEventListener('input', function() {
-            var wrapper = input.closest('.input-wrapper');
-            var showPasswordButton = wrapper.querySelector('.show-password');
-            if (input.value.length > 0) {
-                showPasswordButton.style.fontSize = '16px';
+            if(input.value === '') {
+                // Hide both icons if input is empty
+                showPasswordIcon.style.fontSize = '0px';
+                hidePasswordIcon.style.fontSize = '0px';
             } else {
-                showPasswordButton.style.fontSize = '0px';
+                // Show the appropriate icon based on the input type
+                updatePasswordIcons(input, showPasswordIcon, hidePasswordIcon);
             }
+        });
+
+        showPasswordIcon.addEventListener('click', function() {
+            // When the show password icon is clicked, change the input type to text
+            input.type = 'text';
+            updatePasswordIcons(input, showPasswordIcon, hidePasswordIcon);
+        });
+
+        hidePasswordIcon.addEventListener('click', function() {
+            // When the hide password icon is clicked, change the input type to password
+            input.type = 'password';
+            updatePasswordIcons(input, showPasswordIcon, hidePasswordIcon);
         });
     });
 
-    showPasswords.forEach(function(show, index) {
-        show.addEventListener('click', function() {
-            var passwordInput = show.closest('.input-wrapper').querySelector('input[type="password"]');
-            if (passwordInput && passwordInput.type === 'password') {
-                passwordInput.type = 'text';
-                show.style.fontSize = '0';
-                hidePasswords[index].style.fontSize = '16px';
-            }
-        });
-    });
-
-    hidePasswords.forEach(function(hide, index) {
-        hide.addEventListener('click', function() {
-            var passwordInput = hide.closest('.input-wrapper').querySelector('input[type="text"]');
-            if (passwordInput && passwordInput.type === 'text') {
-                passwordInput.type = 'password';
-                hide.style.fontSize = '0';
-                showPasswords[index].style.fontSize = '16px';
-            }
-        });
-    });
+    function updatePasswordIcons(input, showPasswordIcon, hidePasswordIcon) {
+        if(input.type === 'password') {
+            showPasswordIcon.style.fontSize = '16px';
+            hidePasswordIcon.style.fontSize = '0px';
+        } else {
+            showPasswordIcon.style.fontSize = '0px';
+            hidePasswordIcon.style.fontSize = '16px';
+        }
+    }
 });
