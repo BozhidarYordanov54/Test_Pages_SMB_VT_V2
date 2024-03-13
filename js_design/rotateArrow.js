3f191d313e4e1f7fc7d3aa16790183d96c247f98
Bozhidar Yordanov <108609381+BozhidarYordanov54@users.noreply.github.com>
2024-03-13 19:01:27 +0200

    Added admin page index layout

--- js_design/rotateArrow.js
+++ js_design/rotateArrow.js
@@ -1,12 +1,29 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var menuToggle = document.getElementById("menu-toggle");
-    var arrowDown = document.querySelector(".arrow-down");
+  var menuToggleArticle = document.getElementById("menu-toggle-article");
+  var menuToggleCompetitions = document.getElementById("menu-toggle-competitions");
+  var arrowDowns = document.querySelectorAll(".arrow-down");
 
-    menuToggle.addEventListener("change", function () {
-      if (menuToggle.checked) {
-        arrowDown.classList.add("rotate");
-      } else {
-        arrowDown.classList.remove("rotate");
+  menuToggleArticle.addEventListener("change", function () {
+    if (menuToggleArticle.checked) {
+      arrowDowns[0].classList.add("rotate");
+      if (menuToggleCompetitions.checked) {
+        menuToggleCompetitions.checked = false;
+        arrowDowns[1].classList.remove("rotate");
       }
-    });
+    } else {
+      arrowDowns[0].classList.remove("rotate");
+    }
   });
+
+  menuToggleCompetitions.addEventListener("change", function () {
+    if (menuToggleCompetitions.checked) {
+      arrowDowns[1].classList.add("rotate");
+      if (menuToggleArticle.checked) {
+        menuToggleArticle.checked = false;
+        arrowDowns[0].classList.remove("rotate");
+      }
+    } else {
+      arrowDowns[1].classList.remove("rotate");
+    }
+  });
+});
